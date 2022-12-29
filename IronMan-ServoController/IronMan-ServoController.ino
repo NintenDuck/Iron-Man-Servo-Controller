ea4a588ace5af03721174b5f273d16fcb7c85ede
NintenDuck <andrecked507@gmail.com>
2022-12-29 10:35:42 -0600

    Add: Open/Close with button & Eye leds

--- IronMan-ServoController/IronMan-ServoController.ino
+++ IronMan-ServoController/IronMan-ServoController.ino
@@ -1,4 +1,5 @@
 #include "ServoEasing.hpp"
+#include <time.h>
 
 // ================================================== *
 //			DECLARACION DE VARIABLES
@@ -11,11 +12,13 @@ ServoEasing servo_left;
 
 // ===== Declaracion de pines =====
 
-int BUTTON_PIN					= 1;
-int SERVO_LEFT_PIN				= 2;
-int SERVO_RIGHT_PIN				= 3;
+int SERVO_LEFT_PIN				= 7;
+int SERVO_RIGHT_PIN				= 6;
+int BUTTON_POWER				= 13;
+int BUTTON_PIN					= 5;
+int EYE_PIN						= 3;
 
-int button_pin_data				= 0;
+int button_pin_data				= LOW;
 
 // ===== Variables de control de estados =====
 
@@ -36,17 +39,42 @@ int servo_speed					= 150;
 //			DECLARACION DE METODOS PERSONALES
 // ================================================== *
 
+// inline int randInt(int min, int max) { return (rand() % (max - min)) + min; }
+
+// void randomize_eyes(){
+//   int ranNum = randInt(0, 6);
+//   digitalWrite(EYE_PIN, LOW);
+//   for(int i = 0; i < ranNum; i++) {
+//     digitalWrite(EYE_PIN, HIGH);
+//     delay(randInt(25, 150));
+//     digitalWrite(EYE_PIN, LOW);
+//     delay(randInt(10, 150));
+//   }
+// }
+
+void turn_eyes_on() {
+	digitalWrite( EYE_PIN, HIGH );
+}
+
+void turn_eyes_off(){
+	digitalWrite( EYE_PIN, LOW );
+}
+
 void open_mask() {
-	// Serial.println("Opening mask");
-	servo_right.setEaseTo( 0, servo_speed);
-	servo_left.setEaseTo( 180, servo_speed);
+	Serial.println( "Opening mask" );
+	servo_right.setEaseTo( 0, servo_speed );
+	servo_left.setEaseTo( 180, servo_speed );
 	synchronizeAllServosStartAndWaitForAllServosToStop();
+	current_mask_state = 1;
+	turn_eyes_on();
 }
 void close_mask() {
-	// Serial.println("Closing mask");
+	Serial.println("Closing mask");
+	turn_eyes_off();
 	servo_right.setEaseTo( 180, servo_speed);
 	servo_left.setEaseTo( 0, servo_speed);
 	synchronizeAllServosStartAndWaitForAllServosToStop();
+	current_mask_state = 0;
 }
 
 int get_servo_state() {
@@ -62,37 +90,37 @@ int get_servo_state() {
 // ================================================== *
 
 void setup() {
+  srand(time(0));
 	Serial.begin( 9600 );
 
 	// ===== Setup de boton =====
 	
-	pinMode(BUTTON_PIN, INPUT);
+	pinMode( BUTTON_PIN, INPUT );
+	pinMode( BUTTON_POWER, OUTPUT );
+	pinMode( EYE_PIN, OUTPUT );
+
+	digitalWrite(BUTTON_POWER, HIGH);
+	digitalWrite( EYE_PIN, LOW);
 
 	// ===== Setup de servo motores =====
 
-	servo_left.attach( 	SERVO_LEFT_PIN, 	180 );
-	servo_right.attach(	SERVO_RIGHT_PIN, 	0 );
+	servo_left.attach( 	SERVO_LEFT_PIN, 	0 );
+	servo_right.attach(	SERVO_RIGHT_PIN, 	180 );
 
 	servo_left.setEasingType(	EASE_CUBIC_IN_OUT );
 	servo_right.setEasingType(	EASE_CUBIC_IN_OUT );
-
-	delay(3000);
-	open_mask();
-	delay(3000);
-	close_mask();
 }
 
 
 
 void loop() {
+	delay(1000);
 	button_pin_data = digitalRead(BUTTON_PIN);
 
 	if ( button_pin_data == HIGH ) {
-		// Serial.println("Entered button_pin_data if");
 		current_servo_state = get_servo_state();
 
 		if ( current_servo_state == 0 ) {
-			// Serial.println("Entered current_ser if");
 			if ( current_mask_state == 0 ) {
 				open_mask();
 			}
